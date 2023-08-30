384d000330b6262ab8272dd7d789879e08b059a5
Yahli Mariansky <yahlimem@gmail.com>
2023-08-30 15:40:28 +0300

    ray and line work

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use nannou::prelude::*;
 use nannou_egui::{self, egui, Egui};
+use nannou::math;
 mod ray;
 use ray::Line;
 use ray::Ray;
 
-struct Model<'a> {
+struct Model {
     // window: Window,
     egui: Egui,
     lines: Vec<Line>,
-    rays: Vec<Ray<'a>>,
+    rays: Vec<Ray>,
 }
 
 fn main() {
@@ -20,12 +21,12 @@ fn model(app: &App) -> Model {
     let window_id = app.new_window().view(view).raw_event(raw_window_event).build().unwrap();
     let window = app.window(window_id).unwrap();
     let egui = Egui::from_window(&window);
-    let lines = vec![Line::from(vec2(-100.0, 0.0), vec2(100.0, 0.0), 1.0)];
-    let rays = vec![Ray::from(lines)];
+    let lines = vec![Line::from(vec2(-100.0, -200.0), vec2(100.0, -150.0), 1.0)];
+    let rays = vec![Ray::new()];
     Model {
         egui,
         lines,
-        rays
+        rays,
     }
 }
 
@@ -40,7 +41,9 @@ fn update(app: &App, model: &mut Model, update: Update) {
     });
 
 
-
+    for ray in &mut model.rays {
+        ray.trace((app.mouse.y - ray.pos.y).atan2(app.mouse.x - ray.pos.x), 0.1, &model.lines);
+    }
 }
 
 fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent){
@@ -51,16 +54,21 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
 
-    draw_shapes(&draw, &model.lines);
-    
+    draw_shapes(&draw, &model.rays, &model.lines);
+
     draw.to_frame(app, &frame).unwrap();
     model.egui.draw_to_frame(&frame).unwrap();
 }
 
-fn draw_shapes(draw: &Draw ,lines: &Vec<Line>){
-
+fn draw_shapes(draw: &Draw, rays: &Vec<Ray>, lines: &Vec<Line>){
+    // draw all lines
     for line in lines{
         draw.line().end(line.point1).start(line.point2).weight(line.thickness);
+    }
+
+    for ray in rays {
+        println!("ray point: {}", ray.point);
+        draw.line().end(ray.point).start(ray.pos);
     }
 
 }
--- src/ray.rs
+++ src/ray.rs
@@ -1,28 +1,29 @@
 use nannou::{prelude::*, geom::point};
 
-pub struct Ray<'a>{
+pub struct Ray{
     // pub angle: f32,
     // pub step: f64,
+    pub pos: Vec2,
     pub done: bool,
-    pub all_lines: &'a Vec<Line>,
-    point: Vec2,
+    pub point: Vec2,
     
 }
 
-impl<'a> Ray<'a>{
-    pub fn from(lines: Vec<Line>) -> Self{
-        Self { done: false, point: Vec2::ZERO, all_lines: &lines }
+impl Ray{
+    pub fn new() -> Self{
+        Self { done: false, point: Vec2::ZERO, pos: Vec2::ZERO }
     }
     /// returns true if it hits something.
     /// to access the hit point do Ray.point
-    pub fn trace(&mut self, angle: f32, step: f32) -> bool{
-        let dir = vec2(angle.sin() * step, angle.cos() * step);
+    pub fn trace(&mut self, angle: f32, step: f32, lines: &Vec<Line>) -> bool{
+        self.point = self.pos;
+        let dir = vec2(angle.cos() * step, angle.sin() * step);
         // move loop
-        for _ in 0..(100.0/step) as usize {
+        for _ in 0..(1000.0/step) as usize {
             // move the point in step size and angle
             self.point += dir;
             // check all lines
-            for line in self.all_lines {
+            for line in lines {
                 // find if a line intersects with a point
                 if line.intersect(self.point) {
                     return true;
@@ -47,27 +48,61 @@ impl Line{
     /// point is the points you want to find the intersection with
     /// 
     /// thickness is the minimum distance an intersect will trigger
-    pub fn intersect(&self, point: Vec2) -> bool{
-        
-        let line_m = (self.point1.y - self.point2.y) / (self.point1.x - self.point2.x);
-        // y = mx + b
-        // b = y - mx
-        let line_b = self.point1.y - line_m*self.point1.x;
+    /// written by chat GPT
+    pub fn intersect(&self, point: Vec2) -> bool {
+        let v1 = vec2 (
+            self.point2.x - self.point1.x,
+            self.point2.y - self.point1.y,
+        );
+        let v2 = vec2 (
+            point.x - self.point1.x,
+            point.y - self.point1.y,
+        );
 
-        let distance = (line_m*point.x + 1.0*point.y + line_b).abs() / (line_m*line_m + 1.0).sqrt();
+        let dot_product = v1.x * v2.x + v1.y * v2.y;
+        let squared_length_v1 = v1.x * v1.x + v1.y * v1.y;
 
-        distance <= self.thickness
+        // Calculate the t value along the line segment
+        let t = dot_product / squared_length_v1;
+
+
+        // Check if the intersection point is within the bounds of the line segment
+        if t >= 0.0  && t <= 1.0 {
+            // Calculate the point of intersection on the line
+            let intersection_point = vec2(
+                self.point1.x + t * v1.x,
+                self.point1.y + t * v1.y,
+            );
+
+            // Calculate the distance between the intersection point and the input point
+            let distance = (intersection_point.x - point.x).hypot(intersection_point.y - point.y);
+
+            return distance <= self.thickness;
+        }
+
+        false
     }
+
     
     /// find the distance between this line and a point
-    pub fn distance(&self, point: Vec2) -> f32{
-        let line_m = (self.point1.y - self.point2.y) / (self.point1.x - self.point2.x);
-        // y = mx + b
-        // b = y - mx
-        let line_b = self.point1.y - line_m*self.point1.x;
+    pub fn distance_to_point(&self, point: &Vec2) -> f32 {
+        // Calculate the direction vector of the line
+        let line_direction  = vec2 (
+            self.point2.x - self.point1.x,
+            self.point2.y - self.point1.y,
+        );
+        let point_to_target  = vec2 (
+            point.x - self.point1.x,
+            point.y - self.point1.y,
+        );
 
-        let distance = (line_m*point.x + 1.0*point.y + line_b).abs() / (line_m*line_m + 1.0).sqrt();
+        // Calculate the dot product of the line direction and point_to_target
+        let dot_product = line_direction.x * point_to_target.x + line_direction.y * point_to_target.y;
 
+        // Calculate the distance from the point to the line
+        let distance = dot_product.abs() / line_direction.length();
+
+        // Return the distance
         distance
     }
 }
